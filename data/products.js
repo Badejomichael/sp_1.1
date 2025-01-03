4eeb9afc6fe2c5a85d116ff3456647caa9acfa3b
Michael Badejo <138082642+Badejomichael@users.noreply.github.com>
2025-01-04 00:15:40 +0100

    Add files via upload

---
+++ data/products.js
@@ -0,0 +1,303 @@
+export const FWproducts = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c3",
+    image: "./assets/fw1.jpg",
+    name: "Gown",
+    priceNaira: 20000,
+    keywords: [
+      "female wear",
+      "gowns",
+      "gown",
+      "female wear ",
+      "gowns ",
+      "gown "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c4",
+    image: "./assets/fw5.jpg",
+    name: "Gown",
+    priceNaira: 15000,
+    keywords: [
+      "female wear",
+      "gowns",
+      "gown",
+      "gown",
+      "female wear ",
+      "gowns ",
+      "gown "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c5",
+    image: "./assets/fw3.jpg",
+    name: "Gown",
+    priceNaira: 20000,
+    keywords: [
+      "female wear",
+      "gowns",
+      "gown",
+      "gown",
+      "female wear ",
+      "gowns ",
+      "gown "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c1",
+    image: "./assets/fw4.jpg",
+    name: "Gown",
+    priceNaira: 15000,
+    keywords: [
+      "female wear",
+      "gowns",
+      "gown",
+      "gown",
+      "female wear ",
+      "gowns ",
+      "gown "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c7",
+    image: "./assets/fw11.jpg",
+    name: "Gown",
+    priceNaira: 15000,
+    keywords: [
+      "female wear",
+      "gowns",
+      "gown",
+      "gown",
+      "female wear ",
+      "gowns ",
+      "gown "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678e6",
+    image: "./assets/fw6.jpg",
+    name: "Gown",
+    priceNaira: 20000,
+    keywords: [
+      "female wear",
+      "gowns",
+      "gown",
+      "gown",
+      "female wear ",
+      "gowns ",
+      "gown "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678f1",
+    image: "./assets/fw7.jpg",
+    name: "Gown",
+    priceNaira: 15000,
+    keywords: [
+      "female wear",
+      "gowns",
+      "gown",
+      "gown",
+      "female wear ",
+      "gowns ",
+      "gown "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678f5",
+    image: "./assets/fw13.jpg",
+    name: "Gown",
+    priceNaira: 20000,
+    keywords: [
+      "female wear",
+      "gowns",
+      "gown",
+      "gown",
+      "female wear ",
+      "gowns ",
+      "gown "
+    ]
+  }
+];
+
+export const LIproducts = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678e673",
+    image: "./assets/l1.jpg",
+    name: "Lingerie",
+    priceNaira: 20000,
+    keywords: [
+      "lingeries",
+      "lingerie",
+      "lingeries ",
+      "lingerie "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678f563",
+    image: "./assets/l2.jpg",
+    name: "Lingerie",
+    priceNaira: 15000,
+    keywords: [
+      "lingeries",
+      "lingerie",
+      "lingeries ",
+      "lingerie "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678f531",
+    image: "./assets/l3.jpg",
+    name: "Lingerie",
+    priceNaira: 20000,
+    keywords: [
+      "lingeries",
+      "lingerie",
+      "lingeries ",
+      "lingerie "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c791",
+    image: "./assets/l4.jpg",
+    name: "Lingerie",
+    priceNaira: 15000,
+    keywords: [
+      "lingeries",
+      "lingerie",
+      "lingeries ",
+      "lingerie "
+    ]
+  }
+];
+
+export const MCproducts = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678e6732",
+    image: "./assets/mc1.jpg",
+    name: "Night wear",
+    priceNaira: 20000,
+    keywords: [
+      "couple night wear",
+      "night wear",
+      "matching couple night wear",
+      "night",
+      "couple night wear ",
+      "night wear ",
+      "matching couple night wear ",
+      "night "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678f563e",
+    image: "./assets/mc2.jpg",
+    name: "Night wear",
+    priceNaira: 15000,
+    keywords: [
+      "couple night wear",
+      "night wear",
+      "matching couple night wear",
+      "night",
+      "couple night wear ",
+      "night wear ",
+      "matching couple night wear ",
+      "night "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678f531q",
+    image: "./assets/mc3.jpg",
+    name: "Night wear",
+    priceNaira: 20000,
+    keywords: [
+      "couple night wear",
+      "night wear",
+      "matching couple night wear",
+      "night",
+      "couple night wear ",
+      "night wear ",
+      "matching couple night wear ",
+      "night "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c7911",
+    image: "./assets/mc4.jpg",
+    name: "Night wear",
+    priceNaira: 15000,
+    keywords: [
+      "couple night wear",
+      "night wear",
+      "matching couple night wear",
+      "night",
+      "couple night wear ",
+      "night wear ",
+      "matching couple night wear ",
+      "night "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c152a",
+    image: "./assets/mc5.jpg",
+    name: "Night wear",
+    priceNaira: 15000,
+    keywords: [
+      "couple night wear",
+      "night wear",
+      "matching couple night wear",
+      "night",
+      "couple night wear ",
+      "night wear ",
+      "matching couple night wear ",
+      "night "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678e673d",
+    image: "./assets/mc6.jpg",
+    name: "Night wear",
+    priceNaira: 20000,
+    keywords: [
+      "couple night wear",
+      "night wear",
+      "matching couple night wear",
+      "night",
+      "couple night wear ",
+      "night wear ",
+      "matching couple night wear ",
+      "night "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678f563g",
+    image: "./assets/mc7.jpg",
+    name: "Night wear",
+    priceNaira: 15000,
+    keywords: [
+      "couple night wear",
+      "night wear",
+      "matching couple night wear",
+      "night",
+      "couple night wear ",
+      "night wear ",
+      "matching couple night wear ",
+      "night "
+    ]
+  },
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678f530f",
+    image: "./assets/mc8.jpg",
+    name: "Night wear",
+    priceNaira: 20000,
+    keywords: [
+      "couple night wear",
+      "night wear",
+      "matching couple night wear",
+      "night",
+      "couple night wear ",
+      "night wear ",
+      "matching couple night wear ",
+      "night "
+    ]
+  }
+];
